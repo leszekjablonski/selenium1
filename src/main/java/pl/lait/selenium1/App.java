f6459fda1fa25c530ce38b28ded29790279f422c
Leszek <ljablonski@o2.pl>
2018-08-26 15:02:55 +0200

    26.08.2018

--- src/main/java/pl/lait/selenium1/App.java
+++ src/main/java/pl/lait/selenium1/App.java
@@ -1,10 +1,6 @@
 package pl.lait.selenium1;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-
+@SuppressWarnings("unused")
 public class App 
 {
 	
--- src/test/java/pl/lait/selenium1/Init.java
+++ src/test/java/pl/lait/selenium1/Init.java
@@ -14,19 +14,20 @@ public class Init {
 	static WebDriver driver = null;
 	//komentarz nieistotny dla Jenkinsa
 	public static WebDriver getDriver() {
-		 	System.setProperty("webdriver.gecko.driver", "D:\\geckodriver.exe");
+		 	System.setProperty("webdriver.gecko.driver", "J:\\Pobrane programy\\gecko\\geckodriver.exe");
 		 	URL url = null;
 		 	
 		 	try {
-				url = new URL("http://192.168.8.53:4444/wd/hub");
+				url = new URL("http://192.168.8.59:4444/wd/hub");
 			} catch (MalformedURLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 	        
-	        DesiredCapabilities cap = DesiredCapabilities.chrome();
+	        DesiredCapabilities cap = DesiredCapabilities.firefox();
 	        if (driver == null) {
-	        	driver = new RemoteWebDriver (url, cap);
+	        	driver = new FirefoxDriver();// lokalny firefox, wymaga setProperty (wiersz 17)
+	        	//driver = new RemoteWebDriver (url, cap);//selenium greed, zdalne testy przez hub i node
 		        driver.get("http://newtours.demoaut.com");
 		        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		        //driver.manage().window().maximize();
